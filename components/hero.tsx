b3b01330d8474d9c836bb1d83ff79848db3e2c64
SirSanctified <mambambopritchard@gmail.com>
2024-04-30 08:05:42 +0200

    Add Link component to Hero component for navigation

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,5 +1,6 @@
 import { Button } from "@nextui-org/react";
 import Image from "next/image";
+import Link from "next/link";
 
 const Hero = () => {
   return (
@@ -11,13 +12,21 @@ const Hero = () => {
         <p className="text-lg md:text-2xl mt-4 text-center md:text-start text-gray-600 dark:text-gray-300">
           More than just timekeeping: Discover watches that define you.
         </p>
-        <div className="flex w-full justify-center md:justify-start mt-8">
+        <div className="flex w-full gap-4 flex-col sm:flex-row justify-center md:justify-start items-center mt-8">
           <Button
             size="lg"
             radius="none"
-            className="bg-yellow-800 hover:bg-yellow-600 transition-all duration-300 ease-linear font-medium text-white rounded-lg"
+            className="bg-yellow-800 hover:bg-yellow-600  w-full sm:w-auto transition-all duration-300 ease-linear font-medium text-white rounded-lg"
           >
-            Discover Your Timepiece
+            <Link href={"/products"}>Discover Your Timepiece</Link>
+          </Button>
+          <Button
+            variant="bordered"
+            size="lg"
+            radius="none"
+            className="border-yellow-800 border hover:bg-yellow-600 w-full sm:w-auto transition-all duration-300 ease-linear font-medium rounded-lg"
+          >
+            <Link href={"/contact"}>Speak to Sales</Link>
           </Button>
         </div>
       </div>
