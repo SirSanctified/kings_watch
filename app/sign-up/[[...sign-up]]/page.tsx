f1edc53220642c10d890e405e1e30897a5340ffe
SirSanctified <mambambopritchard@gmail.com>
2024-04-30 16:40:49 +0200

    Add sign-in and sign-up pages with Clerk integration

---
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,16 @@
+import { SignUp } from "@clerk/nextjs";
+import Image from "next/image";
+
+export default function Page() {
+  return (
+    <main className="flex min-h-screen space-y-8 flex-col sm:flex-row sm:space-x-4 md:space-x-8 bg-white dark:bg-gray-800 p-24 px-4 md:px-8 lg:px-24  w-full">
+      <Image
+        src="/hero.svg"
+        alt="hero"
+        width={500}
+        height={500}
+      />
+      <SignUp path="/sign-up" />
+    </main>
+  );
+}
