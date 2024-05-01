d23700a9c0342c8a1a74cd3b74d08fcd2d904884
SirSanctified <mambambopritchard@gmail.com>
2024-05-01 12:30:26 +0200

    Refactor product-utils.ts to use Next.js Sanity client and add functions for fetching products

---
+++ sanity/product-utils.ts
@@ -0,0 +1,39 @@
+import { createClient, groq } from "next-sanity";
+
+const client = createClient({
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
+  apiVersion: "",
+  useCdn: false,
+  token: process.env.SANITY_API_TOKEN,
+});
+
+export async function getProducts() {
+  return await client.fetch(groq`
+    *[_type == "product"] {
+      _id,
+      name,
+      slug: slug.current,
+      image: image.asset->url,
+      price,
+      description,
+      createdAt,
+    }`);
+}
+
+export async function getProductBySlug(slug: string) {
+  return await client.fetch(
+    groq`
+    *[_type == "product" && slug.current == $slug] {
+      _id,
+      name,
+      slug: slug.current,
+      image: image.asset->url,
+      extraImages: extraImages[].asset->url,
+      price,
+      description,
+      colors,
+    }`,
+    { slug }
+  );
+}
