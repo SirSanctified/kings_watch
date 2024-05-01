b349ebe56a0cd2a1bc0cf5c5348019ab40f9fe09
SirSanctified <mambambopritchard@gmail.com>
2024-05-01 11:53:36 +0200

    Add Sanity schema for product document type

---
+++ sanity/schemas/product-schema.ts
@@ -0,0 +1,84 @@
+const product = {
+  name: "product",
+  type: "document",
+  title: "Product",
+  fields: [
+    {
+      name: "name",
+      type: "string",
+      title: "Name",
+      validation: (Rule: { required: () => any }) => Rule.required(),
+    },
+    {
+      name: "slug",
+      type: "slug",
+      title: "Slug",
+      validation: (Rule: { required: () => any }) => Rule.required(),
+      options: {
+        source: "name",
+        maxLength: 200,
+      },
+    },
+    {
+      name: "image",
+      type: "image",
+      title: "Main Image",
+      validation: (Rule: { required: () => any }) => Rule.required(),
+    },
+    {
+      name: "extraImages",
+      type: "array",
+      title: "Extra Images",
+      of: [{ type: "image" }],
+    },
+    {
+      name: "colors",
+      type: "array",
+      title: "Colors",
+      of: [
+        {
+          type: "string",
+          options: {
+            list: [
+              "red",
+              "blue",
+              "green",
+              "yellow",
+              "purple",
+              "black",
+              "white",
+              "gray",
+              "pink",
+              "brown",
+              "orange",
+              "lightblue",
+            ],
+          },
+        },
+      ],
+    },
+    {
+      name: "description",
+      type: "text",
+      title: "Description",
+      validation: (Rule: { required: () => any }) => Rule.required(),
+    },
+    {
+      name: "price",
+      type: "number",
+      title: "Price",
+      validation: (Rule: { required: () => any }) => Rule.required().min(0),
+    },
+    {
+      name: "createdAt",
+      type: "datetime",
+      title: "Created At",
+      options: {
+        dateFormat: "YYYY-MM-DDTHH:mm:ssZ",
+      },
+      readonly: true,
+      initialValue: () => new Date().toISOString(),
+    },
+  ],
+};
+export default product;
