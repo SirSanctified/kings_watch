6ede99083aa2ed13ba941d0ffefff82981bca347
SirSanctified <mambambopritchard@gmail.com>
2024-05-01 11:52:59 +0200

    Update Sanity schema for product document type

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from "sanity";
 import { structureTool } from "sanity/structure";
-import schemas from "./sanity";
+import schemas from "./sanity/schemas";
 
 export default defineConfig({
   name: "default",
