919acb1c4b2ec2544b22531b755df8c065e5136e
SirSanctified <mambambopritchard@gmail.com>
2024-05-02 12:08:33 +0200

    Add category and order schemas, update product and user schemas

--- sanity/schemas/index.ts
+++ sanity/schemas/index.ts
@@ -1,5 +1,10 @@
 import product from "./product-schema";
+import category from "./category-schema";
+import order from "./order-schema";
+import orderItem from "./order-item";
+import cart from "./cart-schema";
+import user from "./user-schema";
 
-const schemas = [product];
+const schemas = [cart, category, order, orderItem, product, user];
 
 export default schemas;
