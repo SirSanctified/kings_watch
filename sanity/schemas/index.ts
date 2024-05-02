262d6ef311d84aad9e208e2b0b90582ad058c2a6
SirSanctified <mambambopritchard@gmail.com>
2024-05-02 15:13:13 +0200

    Remove cart schema and update index.ts

--- sanity/schemas/index.ts
+++ sanity/schemas/index.ts
@@ -2,10 +2,9 @@ import product from "./product-schema";
 import category from "./category-schema";
 import order from "./order-schema";
 import orderItem from "./order-item";
-import cart from "./cart-schema";
 import user from "./user-schema";
 import homeImage from "./home-image";
 
-const schemas = [cart, category, homeImage, order, orderItem, product, user];
+const schemas = [category, homeImage, order, orderItem, product, user];
 
 export default schemas;
