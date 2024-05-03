66191ec52e12ae7a56dbe69c6730cb96a4f91783
SirSanctified <mambambopritchard@gmail.com>
2024-05-03 10:10:29 +0200

    Update dependencies: clsx, tailwind-merge

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,3 +1,10 @@
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
 export function formatCurrency(price: number) {
   return new Intl.NumberFormat("en-US", {
     style: "currency",
