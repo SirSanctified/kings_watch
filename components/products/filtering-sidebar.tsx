611200e3913e1e5f40a2be2e080e31cfd38482b9
SirSanctified <mambambopritchard@gmail.com>
2024-05-06 15:23:14 +0200

    feat: Add filtering functionality to products sidebar

--- components/products/filtering-sidebar.tsx
+++ components/products/filtering-sidebar.tsx
@@ -1,6 +1,45 @@
-import { Button } from "@nextui-org/react";
+"use client";
 
-const FilteringSidebar = () => {
+import { formatCurrency } from "@/lib/utils";
+import { Category } from "@/types";
+import { Button } from "@nextui-org/react";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
+const FilteringSidebar = ({
+  category,
+  price,
+  sortBy,
+  categories,
+}: {
+  category?: string;
+  price?: number;
+  sortBy?: string;
+  categories: Category[];
+}) => {
+  const [sortByFilter, setSortByFilter] = useState<string>(sortBy || "newest");
+  const [categoryFilter, setCategoryFilter] = useState<string>(category || "");
+  const [priceFilter, setPriceFilter] = useState<number>(price || 0);
+
+  const router = useRouter();
+
+  const handleFilter = () => {
+    const url = new URL(window.location.href);
+    url.searchParams.set("category", categoryFilter);
+    url.searchParams.set("price", priceFilter.toString());
+    url.searchParams.set("sortBy", sortByFilter);
+    if (categoryFilter === "") {
+      url.searchParams.delete("category");
+    }
+    if (priceFilter.toString() === "0") {
+      url.searchParams.delete("price");
+    }
+    if (sortByFilter === "") {
+      url.searchParams.delete("sortBy");
+    }
+    router.push(url.toString());
+  };
+
   return (
     <div className="flex flex-wrap gap-6">
       <div className="w-auto md:w-full flex flex-col space-y-1">
@@ -13,15 +52,21 @@ const FilteringSidebar = () => {
         <select
           id="category"
           name="category"
+          value={categoryFilter}
+          onChange={(e) => setCategoryFilter(e.target.value)}
           className="max-w-40 md:max-w-md w-full p-2 border border-gray-300 rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
         >
-          <option value="all">All</option>
-          <option value="men">Men</option>
-          <option value="women">Women</option>
-          <option value="kids">Kids</option>
+          <option value="">All</option>
+          {categories.map((category) => (
+            <option
+              key={category._id}
+              value={category._id}
+            >
+              {category.name}
+            </option>
+          ))}
         </select>
       </div>
-      {/* By Price */}
       <div className="w-auto md:w-full flex flex-col space-y-1">
         <label
           htmlFor="price"
@@ -29,17 +74,20 @@ const FilteringSidebar = () => {
         >
           Price
         </label>
+        <span className="text-sm text-gray-500 dark:text-gray-400">
+          {formatCurrency(priceFilter)}
+        </span>
         <input
           id="price"
           name="price"
           type="range"
-          min="0"
-          max="100"
+          value={priceFilter}
+          onChange={(e) => setPriceFilter(Number(e.target.value))}
+          max="200"
           step="10"
           className="max-w-40 md:max-w-md w-full p-2 border border-gray-300 rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
         />
       </div>
-      {/* By oldest/newest */}
       <div className="w-auto md:w-full flex flex-col space-y-1">
         <label
           htmlFor="sorting"
@@ -50,14 +98,18 @@ const FilteringSidebar = () => {
         <select
           id="sorting"
           name="sorting"
+          value={sortByFilter}
+          onChange={(e) => setSortByFilter(e.target.value)}
           className="max-w-40 md:max-w-md w-full p-2 border border-gray-300 rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
         >
-          <option value="">Select an option</option>
           <option value="newest">Newest</option>
           <option value="oldest">Oldest</option>
         </select>
       </div>
-      <Button className="w-full text-white text-lg font-semibold bg-yellow-700 hover:bg-yellow-800 focus:ring-4 focus:ring-yellow-300 rounded-lg px-5 py-2.5 dark:bg-yellow-600 dark:hover:bg-yellow-700 focus:outline-none dark:focus:ring-yellow-800 transition-all duration-300 ease-linear">
+      <Button
+        onClick={handleFilter}
+        className="w-full text-white text-lg font-semibold bg-yellow-700 hover:bg-yellow-800 focus:ring-4 focus:ring-yellow-300 rounded-lg px-5 py-2.5 dark:bg-yellow-600 dark:hover:bg-yellow-700 focus:outline-none dark:focus:ring-yellow-800 transition-all duration-300 ease-linear"
+      >
         Apply Filters
       </Button>
     </div>
--- components/products/products-container.tsx
+++ components/products/products-container.tsx
@@ -2,13 +2,34 @@ import { Product } from "@/app/(root)/page";
 import FilteringSidebar from "./filtering-sidebar";
 import ProductsListings from "./product-listings";
 import { getProducts } from "@/sanity/product-utils";
+import { Category } from "@/types";
+import { getCategories } from "@/sanity/category-utils";
 
-export default async function ProductsContainer() {
-  const products: Product[] = await getProducts();
+export default async function ProductsContainer({
+  category,
+  price,
+  sortBy,
+}: {
+  category?: string;
+  price?: number;
+  sortBy?: string;
+}) {
+  const products: Product[] = await getProducts(
+    category ?? "",
+    price ?? 0,
+    sortBy ?? "newest"
+  );
+  const categories: Category[] = await getCategories();
+
   return (
     <div className="w-full flex gap-6 flex-col my-6 md:flex-row">
       <div className="md:w-1/3 lg:w-1/4">
-        <FilteringSidebar />
+        <FilteringSidebar
+          category={category}
+          categories={categories}
+          price={price}
+          sortBy={sortBy}
+        />
       </div>
       <div className="md:w-2/3 lg:w-3/4 flex flex-col w-full items-start justify-start">
         <ProductsListings products={products} />
