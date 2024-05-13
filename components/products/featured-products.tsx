7c1590cd9b19ceb6a9c6f637083c8b024e331b76
SirSanctified <mambambopritchard@gmail.com>
2024-05-13 14:05:02 +0200

    chore: Update featured products layout and styles

--- components/products/featured-products.tsx
+++ components/products/featured-products.tsx
@@ -6,16 +6,16 @@ const FeaturedProducts = ({ products }: { products: Product[] }) => {
   return (
     <section className="flex flex-col items-center justify-start gap-4 px-4 md:px-8 lg:px-24 w-full">
       <div className="flex items-center justify-between gap-4 w-full">
-      <h1 className="text-2xl md:text-4xl text-start font-bold text-gray-800 dark:text-white">
-        Featured Products
-      </h1>
-      <Link
-        href="/products"
-        title="View All Products"
-        className="flex-1 whitespace-nowrap text-gray-600 hover:text-yellow-600 dark:text-gray-300 transition-all duration-300 ease-linear"
-      >
-        See All
-      </Link>
+        <h1 className="text-2xl w-full md:text-4xl text-start font-bold text-gray-800 dark:text-white">
+          Featured Products
+        </h1>
+        <Link
+          href="/products"
+          title="View All Products"
+          className="whitespace-nowrap text-gray-600 hover:text-yellow-600 dark:text-gray-300 transition-all duration-300 ease-linear"
+        >
+          See All
+        </Link>
       </div>
 
       <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 w-full mb-8">
