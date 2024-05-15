6c07bca0cee4abd494cdac3cb5c40748c47c4404
kingswatchzim <kingswatchzim@gmail.com>
2024-05-15 12:09:22 +0000

    fix fetching to exclude drafts

--- components/products/products-container.tsx
+++ components/products/products-container.tsx
@@ -3,23 +3,46 @@ import ProductsListings from "./product-listings";
 import { getProducts } from "@/sanity/product-utils";
 import { Category } from "@/types";
 import { getCategories } from "@/sanity/category-utils";
+import { Product } from "@/app/(root)/page";
 
-export default async function ProductsContainer({
-  category,
-  price,
-  sortBy,
-}: {
+export async function getStaticProps(props: {
   category?: string;
   price?: number;
   sortBy?: string;
 }) {
-  const products = await getProducts(
+  const { category, price, sortBy } = props;
+
+  const products = getProducts(
     category ?? "",
     price ?? 0,
     sortBy ?? "newest"
   );
   const categories: Category[] = await getCategories();
 
+  return {
+    props: {
+      products,
+      categories,
+    },
+    revalidate: 300, 
+  };
+}
+
+
+export default async function ProductsContainer({
+  category,
+  price,
+  sortBy,
+  categories,
+  products,
+}: {
+  category?: string;
+  price?: number;
+  sortBy?: string;
+  categories: Category[],
+  products: Product[]
+}) {
+  
   return (
     <div className="w-full flex gap-6 flex-col my-6 md:flex-row">
       <div className="md:w-1/3 lg:w-1/4">
