8d23fb8b1a449d92071776726f2d262f206475f8
kingswatchzim <kingswatchzim@gmail.com>
2024-05-15 12:15:36 +0000

    Remove the static props

--- components/products/products-container.tsx
+++ components/products/products-container.tsx
@@ -3,46 +3,23 @@ import ProductsListings from "./product-listings";
 import { getProducts } from "@/sanity/product-utils";
 import { Category } from "@/types";
 import { getCategories } from "@/sanity/category-utils";
-import { Product } from "@/app/(root)/page";
 
-export async function getStaticProps(props: {
+export default async function ProductsContainer({
+  category,
+  price,
+  sortBy,
+}: {
   category?: string;
   price?: number;
   sortBy?: string;
 }) {
-  const { category, price, sortBy } = props;
-
-  const products = getProducts(
+  const products = await getProducts(
     category ?? "",
     price ?? 0,
     sortBy ?? "newest"
   );
   const categories: Category[] = await getCategories();
 
-  return {
-    props: {
-      products,
-      categories,
-    },
-    revalidate: 300, 
-  };
-}
-
-
-export default async function ProductsContainer({
-  category,
-  price,
-  sortBy,
-  categories,
-  products,
-}: {
-  category?: string;
-  price?: number;
-  sortBy?: string;
-  categories: Category[],
-  products: Product[]
-}) {
-  
   return (
     <div className="w-full flex gap-6 flex-col my-6 md:flex-row">
       <div className="md:w-1/3 lg:w-1/4">
